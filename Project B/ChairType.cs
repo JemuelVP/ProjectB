30821b01e70514265e5bc6a4376e5ff573671b57
CS0nder <152660399+CS0nder@users.noreply.github.com>
2024-03-14 21:34:19 +0100

    Update ChairType.cs

--- Project B/ChairType.cs
+++ Project B/ChairType.cs
@@ -12,7 +12,46 @@ class ChairType
     }
 
 
-    // willen jullie hier dan methods zodat hij chekct welke tppe stoel hij heeft zodat
-    //er dan bijgehouden kan worden door aan een lijst toe te voegen of een count kan maken zodat er later kan worden opgeteld hoeveel tickets er zijn verkocht voor elke seat 
+    //manier 1 
+
+    public int LoveSeat = 0;
+    public int ExtraLegRoomSeat = 0;
+    public int NormalSeat = 0;
+    public void CheckChairtype(ChairType chairtype)
+    {
+        if (chairtype.Name.ToLower() == "Loveseat")
+        {   
+            LoveSeat++;
+        }
+        else if (chairtype.Name.ToLower() == "ExtraLegRoomSeat")
+        {
+            ExtraLegRoomSeat++;
+        }
+        else if (chairtype.Name.ToLower() == "NormalSeat")
+        {
+            NormalSeat++;
+        }
+    }
+
+    // manier 2
+    public List<ChairType> listOfLoveseats = new List<ChairType>();
+    public List<ChairType> listOfExtraLegRoomSeats = new List<ChairType>();
+    public List<ChairType> listOfNormalSeats = new List<ChairType>();
+
+    public void CheckChair(ChairType chairtype)
+    {
+        if (chairtype.Name.ToLower() == "Loveseat")
+        {   
+            listOfLoveseats.Add(chairtype);
+        }
+        else if (chairtype.Name.ToLower() == "ExtraLegRoomSeat")
+        {
+            listOfExtraLegRoomSeats.Add(chairtype);
+        }
+        else if (chairtype.Name.ToLower() == "NormalSeat")
+        {
+            listOfNormalSeats.Add(chairtype);
+        }
+    }
 }
 
