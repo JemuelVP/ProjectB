096bdaf4e3af3eeb772448e3f8c9009bea49fd92
CS0nder <152660399+CS0nder@users.noreply.github.com>
2024-03-14 21:14:08 +0100

    Create ChairType.cs

---
+++ Project B/ChairType.cs
@@ -0,0 +1,18 @@
+class ChairType
+{
+    public int Id;
+    public string Name;
+    public double Price;
+
+    public ChairType(int id, string name,double price)
+    {
+        Id = id;
+        Name = name;
+        Price = price;
+    }
+
+
+    // willen jullie hier dan methods zodat hij chekct welke tppe stoel hij heeft zodat
+    //er dan bijgehouden kan worden door aan een lijst toe te voegen of een count kan maken zodat er later kan worden opgeteld hoeveel tickets er zijn verkocht voor elke seat 
+}
+
