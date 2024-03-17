0e324eb1b41e2b021c090cfbfea8eabd3a78714c
JemuelVP <jemuelvalpoort10@gmail.com>
2024-03-17 21:24:09 +0100

    added login functionality and database class

---
+++ Project B/Admin.cs
@@ -0,0 +1,34 @@
+using System.Data.SQLite;
+
+
+public class Admin {
+
+    public string? Name;
+    public string? Password;
+    public bool LoggedIn = false;
+
+    public void Login(string name, string password) {
+        string connectionString = "Data Source=mydatabase.sqlite;Version=3;";
+        using SQLiteConnection connection = new SQLiteConnection(connectionString);
+        connection.Open();
+        string selectQuery = "SELECT * FROM Admin";
+        using SQLiteCommand selectCommand = new SQLiteCommand(selectQuery, connection);
+
+        using SQLiteDataReader reader = selectCommand.ExecuteReader();
+        if (reader.Read())
+        {
+            string? storedName = reader["Name"].ToString();
+            string? storedPassword = reader["Password"].ToString();
+            if (name == storedName && password == storedPassword)
+            {
+                Console.WriteLine("Succesvol ingelogd");
+                LoggedIn = true;
+            }
+            else 
+            {
+                Console.WriteLine("Verkeerde gegevens ingevuld probeer het opnieuw");
+            }
+        }
+        connection.Close();
+    }
+}
