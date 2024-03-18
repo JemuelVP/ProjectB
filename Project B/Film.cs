bd51cd593401164d78add6a3a7e08bbe022d575c
Mhammadarab <86677096+Mhammadarab@users.noreply.github.com>
2024-03-18 15:59:13 +0100

    Insert Movie into database

--- Project B/Film.cs
+++ Project B/Film.cs
@@ -1,3 +1,5 @@
+using System.Data.SQLite;
+
 class Film
 {
     public int ID { get; set; }
@@ -11,4 +13,23 @@ class Film
     public required int Age { get; set; }
     public required int DurationInMin { get; set; }
     
+    public void OverviewMovies(string title, int year)
+    {
+        string connectionString = "Data Source=movie.sqlite;Version=3;";
+        using SQLiteConnection connection = new SQLiteConnection(connectionString);
+        connection.Open();
+        string selectQuery = $"INSERT INTO  Movie (Title, Year, Price, Description, Authors, Categories, Directors, Age, DurationInMin) VALUES (Spiderman, 2002, 200, THE BOMB, VALPOORT AND ARAB, ACTION, VALPOORT AND ARAB, 36, 120) ";
+        using SQLiteCommand insertCommand = new SQLiteCommand(selectQuery, connection);
+
+        using SQLiteDataReader reader = insertCommand.ExecuteReader();
+        // if (reader.Read())
+        // {
+        //     string? storedTitle = reader["Title"].ToString();
+        //     string? storedYear = reader["Year"].ToString();
+        //     Console.WriteLine($"{storedTitle}");
+        //     Console.WriteLine($"{storedYear}");
+
+        // }
+        
+    }
 }
