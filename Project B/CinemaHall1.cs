6182688943c2da3d9bd3e313e955f5e964b613d6
ELENAJACOBS_2017@YAHOO.COM <146252282+ECarolinaJacobs@users.noreply.github.com>
2024-03-18 21:08:56 +0100

    CinemaHallDebugged

--- Project B/CinemaHall1.cs
+++ Project B/CinemaHall1.cs
@@ -1,9 +1,10 @@
-class CinemaHall1 : CinemaHall 
+class CinemaHall1 : CinemaHall
 {
     public CinemaHall1(int id) : base(id, 150)
     {
-        FillChairs();
+        FillChairs(id, "name", 20.0);
     }
 }
 // derived from Cinemahall, here the list gets filled with chairs throug method fillChairs(), until the max seats 
 // are reached.
+// added placeholder parameters to FillChairs()
--- Project B/CinemaHall3.cs
+++ Project B/CinemaHall3.cs
@@ -1,9 +1,10 @@
 class CinemaHall3 : CinemaHall 
 {
-    public CinemaHall1(int id) : base(id, 500)
+    public CinemaHall3(int id) : base(id, 500)
     {
-        FillChairs();
+        FillChairs(id, "name", 27.0);
     }
 }
 // derived from Cinemahall, here the list gets filled with chairs throug method fillChairs(), until the max seats 
 // are reached.
+// added placeholder parameters to FillChairs()
