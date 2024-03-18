8ebd63bfe46b973af3517edc8fad81634f50a140
ELENAJACOBS_2017@YAHOO.COM <146252282+ECarolinaJacobs@users.noreply.github.com>
2024-03-18 21:34:43 +0100

    CinemaHall3 25.0

--- Project B/CinemaHall3.cs
+++ Project B/CinemaHall3.cs
@@ -2,7 +2,7 @@ class CinemaHall3 : CinemaHall
 {
     public CinemaHall3(int id) : base(id, 500)
     {
-        FillChairs(id, "name", 27.0);
+        FillChairs(id, "name", 25.0);
     }
 }
 // derived from Cinemahall, here the list gets filled with chairs throug method fillChairs(), until the max seats 
