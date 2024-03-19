27ce49ce6858f764ead87f527a19669345928848
JemuelVP <jemuelvalpoort10@gmail.com>
2024-03-19 13:10:44 +0100

    revenue class

--- Project B/Chair.cs
+++ Project B/Chair.cs
@@ -3,14 +3,14 @@ public class Chair
     public int Id;
     public string Name;
     public double Price;
-    
 
-    public Chair(int id, string name,double price)
+    public bool Sold;
+
+    public Chair(int id, string name, double price)
     {
         Id = id;
         Name = name;
         Price = price;
+        Sold = false;
     }
-
 }
-
