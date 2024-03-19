3266a30f046efde479b7ad3b462e1e64a3c3e6af
Mhammadarab <mohammadarab044@gmail.com>
2024-03-19 13:16:42 +0100

    DisplayMovieTitle and DisplayMovieInfo

--- Project B/Program.cs
+++ Project B/Program.cs
@@ -11,3 +11,18 @@ if (admin.LoggedIn == true) {
 else {
     Console.WriteLine("niet ingelogd");
 }
+
+Film film = new Film();
+
+film.DisplayMovieTitle();
+
+Console.Write("Enter the title of the movie: ");
+string movieTitle = Console.ReadLine();
+
+film.DisplayMovieInfo(movieTitle);
+
+// Call the OverviewMovies method to insert movie data into the database
+// film.OverviewMovies("Batman", 2002, 200, "THE BOMB", "VALPOORT AND ARAB", "ACTION", "VALPOORT AND ARAB", 36, 120);
+
+// // Print a message indicating successful data insertion
+// Console.WriteLine("Data inserted successfully.");
