8fcdfe3dc0791b0eb5dbcd274c987ea7df0ada7c
CS0nder <152660399+CS0nder@users.noreply.github.com>
2024-03-21 12:37:06 +0100

    adjust chair price based on chair type

--- Project B/Chair.cs
+++ Project B/Chair.cs
@@ -13,4 +13,19 @@ public class Chair
         Price = price;
         Sold = false;
     }
+
+
+    //Checks which type of Chair is chosen and adjusts the Chair Price based on the Chair Name.
+    public void PriceForChairType(Chair chair)
+    {
+        if (chair.Name.ToLower() == "loveseat")
+        {
+            chair.Price += 5;
+        }
+        else if (chair.Name.ToLower() == "extralegroom")
+        {   
+            // There is no price specified for the extralegroom chair so i just gave it a 2.5 for now, we can adjust it later.
+            chair.Price += 2.5;
+        }
+    }
 }
