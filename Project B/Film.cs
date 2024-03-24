2b98e8e8c70df54f3e5630441ebc42fb0c298ee7
Mhammadarab <mohammadarab044@gmail.com>
2024-03-24 17:00:58 +0100

    MoviesOverview and new use of ef

--- Project B/Film.cs
+++ Project B/Film.cs
@@ -12,102 +12,31 @@ class Film
     public int Age { get; set; }
     public int DurationInMin { get; set; }
 
-    // Method to insert movie data into the database
-    // public void OverviewMovies(string title, int year, int price, string description, string authors, string categories, string directors, int age, int durationInMin)
-    // {
-    //     // Connection string to connect to the SQLite database
-    //     string connectionString = "Data Source=movie.sqlite;Version=3;";
-
-    //     // Using statement ensures the connection is closed properly after use
-    //     using (SQLiteConnection connection = new SQLiteConnection(connectionString))
-    //     {
-    //         connection.Open();
-
-    //         // SQL query to insert data into the Movie table
-    //         string insertQuery = "INSERT INTO Movie (Title, Year, Price, Description, Authors, Categories, Directors, Age, DurationInMin) " +
-    //                              "VALUES (@Title, @Year, @Price, @Description, @Authors, @Categories, @Directors, @Age, @DurationInMin)";
-    //         // Using the '@' symbol before the "" so you can include the '' without escaping them
-    //         // Using statement ensures the command is disposed properly after use
-    //         using (SQLiteCommand insertCommand = new SQLiteCommand(insertQuery, connection))
-    //         {
-    //             insertCommand.Parameters.AddWithValue("@Title", title);
-    //             insertCommand.Parameters.AddWithValue("@Year", year);
-    //             insertCommand.Parameters.AddWithValue("@Price", price);
-    //             insertCommand.Parameters.AddWithValue("@Description", description);
-    //             insertCommand.Parameters.AddWithValue("@Authors", authors);
-    //             insertCommand.Parameters.AddWithValue("@Categories", categories);
-    //             insertCommand.Parameters.AddWithValue("@Directors", directors);
-    //             insertCommand.Parameters.AddWithValue("@Age", age);
-    //             insertCommand.Parameters.AddWithValue("@DurationInMin", durationInMin);
-
-    //             // Execute the command to insert data into the database
-    //             insertCommand.ExecuteNonQuery();
-    //         }
-    //     }
-    // }
-    public void DisplayMovieTitle()
+    public static List<Film> GetAllMovies()
     {
-        string connectionString = "Data Source=movie.sqlite;Version=3;";
+        using DataBaseConnection db = new();
+        var films = db.Movie.ToList();
 
-        using (SQLiteConnection connection = new SQLiteConnection(connectionString))
-        {
-            connection.Open();
-
-            string selectQuery = "SELECT Title, Year From Movie";
-            using (SQLiteCommand selectCommand = new SQLiteCommand(selectQuery, connection))
-            {
-                using (SQLiteDataReader reader = selectCommand.ExecuteReader())
-                {
-                    if(reader.HasRows)
-                    {
-                        Console.WriteLine("Films: ");
-                        while (reader.Read())
-                        {
-                            string? title = reader["Title"].ToString();
-                            int year = Convert.ToInt32(reader["Year"]);
-                            System.Console.WriteLine($"{title}, {year}");
-                        }
-                    }
-                }
-            }
-        }
+        return films;
     }
 
-    public void DisplayMovieInfo(string? movieTitle)
+    public static Film? GetMovieByTitleAndYear(string title, int year)
     {
-        string connectionString = "Data Source=movie.sqlite;Version=3;";
-
-        using (SQLiteConnection connection = new SQLiteConnection(connectionString))
-        {
-            connection.Open();
-
-            string selectQuery = "SELECT * From Movie WHERE Title = @Title ";
-            using (SQLiteCommand selectCommand = new SQLiteCommand(selectQuery, connection))
-            {
-                // Add parameter for the movie title
-                selectCommand.Parameters.AddWithValue("@Title", movieTitle);
-                
-                using (SQLiteDataReader reader = selectCommand.ExecuteReader())
-                {
-                    if(reader.Read())
-                    {
-                        Console.WriteLine("Movie Information:");
-                        Console.WriteLine($"Title: {reader["Title"]}");
-                        Console.WriteLine($"Year: {reader["Year"]}");
-                        Console.WriteLine($"Price: {reader["Price"]}");
-                        Console.WriteLine($"Description: {reader["Description"]}");
-                        Console.WriteLine($"Authors: {reader["Authors"]}");
-                        Console.WriteLine($"Categories: {reader["Categories"]}");
-                        Console.WriteLine($"Directors: {reader["Directors"]}");
-                        Console.WriteLine($"Age: {reader["Age"]}");
-                        Console.WriteLine($"Duration (minutes): {reader["DurationInMin"]}");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Movie not found.");
-                    }
-                }
-            }
-        }
+        using DataBaseConnection db = new();
+        var filmInfo = db.Movie.FirstOrDefault(film => film.Title == title && film.Year == year);
+        return filmInfo;
+    }
+    public void Display(Film film)
+    {
+        Console.WriteLine("Movie Information:");
+        Console.WriteLine($"Title: {film.Title}");
+        Console.WriteLine($"Year: {film.Year}");
+        Console.WriteLine($"Price: {film.Price}");
+        Console.WriteLine($"Description: {film.Description}");
+        Console.WriteLine($"Authors: {film.Authors}");
+        Console.WriteLine($"Categories: {film.Categories}");
+        Console.WriteLine($"Directors: {film.Directors}");
+        Console.WriteLine($"Age: {film.Age}");
+        Console.WriteLine($"Duration (minutes): {film.DurationInMin}");
     }
 }
--- Project B/Program.cs
+++ Project B/Program.cs
@@ -1,75 +1,104 @@
-﻿DatabaseConnection.CreateDatabase();
+﻿using Spectre.Console;
 Film film = new Film();
 Admin admin = new Admin();
 bool active = true;
-
 Console.WriteLine("Welkom bij YourEyes");
-while(active)
+while (active)
 {
-    Console.WriteLine("Bent u een admin kies dan 1 om in te loggen");
-    Console.WriteLine("Bent u een klant kies dan 2");
-    Console.WriteLine("Om te stoppen kunt u op elk moment q invoeren");
-    string? activeChoice = Console.ReadLine();
-    if(activeChoice.ToLower() == "q" )
+    var MainMenuOption = AnsiConsole.Prompt(new SelectionPrompt<MainMenuOptions>().Title("Bent u een admin of een klant").AddChoices(
+    MainMenuOptions.Admin,
+    MainMenuOptions.Customer));
+
+    switch (MainMenuOption)
     {
-        active = false;
-    }
-    else if (Convert.ToInt32(activeChoice) == 1)
-    {
-        Console.WriteLine("Voer je naam in");
-        string? name = Console.ReadLine();
-        Console.WriteLine("Voer je wachtwoord in");
-        string? password = Console.ReadLine();
-        admin.Login(name, password);
-        if (admin.LoggedIn == true)
-        {
-            Console.WriteLine("hallo");
-        }
-        else
-        {
-            Console.WriteLine("niet ingelogd");
-        }
-    }
-    else if(Convert.ToInt32(activeChoice) == 2)
-    {
-        // Film Overview
-        Console.WriteLine("Om beschikbare films te bekijken kies dan 1");
-        System.Console.WriteLine("Om de films die binnenkort uitkomen kies 2");
-        int customersChoice = Convert.ToInt32(Console.ReadLine());
-        if(customersChoice == 1)
-        {
-            film.DisplayMovieTitle();
-            Console.Write("Voer de titel van de film in: ");
-            string movieTitle = Console.ReadLine();
-            if(movieTitle !="")
+        case MainMenuOptions.Admin:
+            Console.WriteLine("Voer je naam in");
+            string? name = Console.ReadLine();
+            Console.WriteLine("Voer je wachtwoord in");
+            string? password = Console.ReadLine();
+            admin.Login(name, password);
+            if (admin.LoggedIn == true)
             {
-                film.DisplayMovieInfo(movieTitle);
+                Console.WriteLine("hallo");
             }
-            
-            Console.WriteLine("Om een resevering te maken voer 1 in");
-            
-        }
-        else if(customersChoice == 2)
-        {
+            else
+            {
+                Console.WriteLine("niet ingelogd");
+            }
+            break;
+        case MainMenuOptions.Customer:
+            var ReservationMenuOption = AnsiConsole.Prompt(new SelectionPrompt<ReservationMenuOptions>().Title("Beschikbare films").AddChoices(
+            ReservationMenuOptions.OverviewMovies,
+            ReservationMenuOptions.SearchMovies,
+            ReservationMenuOptions.SelectMovie,
+            ReservationMenuOptions.Back,
+            ReservationMenuOptions.Quit));
+            switch (ReservationMenuOption)
+            {
+                case ReservationMenuOptions.OverviewMovies:
+                    var films = Film.GetAllMovies();
+                    string[] movieInfoArray = films
+                                    .Select(book => $"Title: {book.Title}, Year: {book.Year}")
+                                    .ToArray();
+                    var overviewMenu = AnsiConsole.Prompt(new SelectionPrompt<string>().Title("Kies een film").AddChoices(movieInfoArray));
+                    // Split the string by ','
+                    string[] parts = overviewMenu.Split(',');
 
-        }
-    }
-    else
-    {
-        Console.WriteLine("Verkeerde input probeer het opnieuw");
-    }
+                    // Extract title and year
+                    string title = parts[0].Split(':')[1].Trim();
+                    int year = int.Parse(parts[1].Split(':')[1].Trim());
+                    var selectedMovie = Film.GetMovieByTitleAndYear(title, year);
+                    if (selectedMovie == null)
+                    {
+                        break;
+                    }
+                    selectedMovie.Display(selectedMovie);
+                    Console.ReadKey();
+                    break;
+            }
 
+            break;
+
+    }
 }
+
+
+// while(active)
+// {
+//     Console.WriteLine("Bent u een admin kies dan 1 om in te loggen");
+//     Console.WriteLine("Bent u een klant kies dan 2");
+//     Console.WriteLine("Om te stoppen kunt u op elk moment q invoeren");
+//     string? activeChoice = Console.ReadLine();
+//     if(activeChoice.ToLower() == "q" )
+//     {
+//         active = false;
+//     }
+//     else if (Convert.ToInt32(activeChoice) == 1)
+//     {
+//        
+//     }
+//     else if(Convert.ToInt32(activeChoice) == 2)
+//     {
+//         // Film Overview
+//        
+//     }
+//     else
+//     {
+//         Console.WriteLine("Verkeerde input probeer het opnieuw");
+//     }
+
+// }
 public enum MainMenuOptions
 {
     Admin,
-    Customer 
+    Customer
 }
 
 public enum ReservationMenuOptions
 {
-    Apple,
-    Banana,
-    Orange,
-    Grape
+    OverviewMovies,
+    SearchMovies,
+    SelectMovie,
+    Back,
+    Quit
 }
