02601d74936999f018e5a1ed7f5c0cffa01540da
Mhammadarab <mohammadarab044@gmail.com>
2024-05-15 13:11:48 +0200

    add migrations for cinema hall

---
+++ Project B/Migrations/20240514174149_InitialCreate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectB.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>("Size","Hall", nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn("Size","Hall");
+        }
+    }
+}
