e03da73f18083ee60e0d3dac84e53902ae8a8d82
JemuelVP <jemuelvalpoort10@gmail.com>
2024-05-21 19:38:51 +0200

    User gets a discount when many times visited and fixed username when logged in

--- Project B/Ticket.cs
+++ Project B/Ticket.cs
@@ -25,21 +25,29 @@ public class Ticket
         User_ID = userId ?? 0; // Assign User_ID if it's provided, otherwise use 0 or any other default value
 
         using DataBaseConnection db = new();
-        var entry = db.Ticket.Add(new Ticket
-        {
-            Schedule_ID = schedule.ID,
-            Chair_ID = chair_ID,
-            Movie_ID = movieId,
-            Price = price,
-            User_ID = userId ?? 0,
-            DateBought = DateTime.Now
-        });
+        var entry = db.Ticket.Add(
+            new Ticket
+            {
+                Schedule_ID = schedule.ID,
+                Chair_ID = chair_ID,
+                Movie_ID = movieId,
+                Price = price,
+                User_ID = userId ?? 0,
+                DateBought = DateTime.Now
+            }
+        );
         db.SaveChanges();
 
         return price;
     }
 
-    public double GetSeatPrice(int seatType, int seatrow, int seatcol, Schedule schedule, bool qualifyForDiscount)
+    public double GetSeatPrice(
+        int seatType,
+        int seatrow,
+        int seatcol,
+        Schedule schedule,
+        bool qualifyForDiscount
+    )
     {
         // Here you can implement your pricing logic based on seat type, seat number, and schedule
         // For demonstration purposes, let's say you have a simple pricing logic
@@ -48,45 +56,63 @@ public class Ticket
 
         // Adjust price based on seat type
         // This is for the red area
-        if
-        (
-            schedule.Hall_ID == 1 && 
-            seatType == 1 && seatrow >= 5 && seatrow <= 8 && seatcol >= 5 && seatcol <= 6 ||
-            schedule.Hall_ID == 2 && 
-            seatrow >= 5 && seatrow <= 12  && seatcol >= 8 && seatcol <= 9||
-            seatrow >= 6 && seatrow <= 11 && seatcol >= 7 && seatcol <= 10||
-            seatrow >= 7 && seatrow <= 10 && seatcol >= 6 && seatcol <= 11||
-            schedule.Hall_ID == 3 && 
-            seatrow >= 4 && seatrow <= 12 && seatcol >= 13 && seatcol <= 16||
-            seatrow >= 5 && seatrow <= 11 && seatcol >= 12 && seatcol <= 17||
-            seatrow >= 6 && seatrow <= 11 && seatcol >= 11 && seatcol <= 18
+        if (
+            schedule.Hall_ID == 1
+                && seatType == 1
+                && seatrow >= 5
+                && seatrow <= 8
+                && seatcol >= 5
+                && seatcol <= 6
+            || schedule.Hall_ID == 2
+                && seatrow >= 5
+                && seatrow <= 12
+                && seatcol >= 8
+                && seatcol <= 9
+            || seatrow >= 6 && seatrow <= 11 && seatcol >= 7 && seatcol <= 10
+            || seatrow >= 7 && seatrow <= 10 && seatcol >= 6 && seatcol <= 11
+            || schedule.Hall_ID == 3
+                && seatrow >= 4
+                && seatrow <= 12
+                && seatcol >= 13
+                && seatcol <= 16
+            || seatrow >= 5 && seatrow <= 11 && seatcol >= 12 && seatcol <= 17
+            || seatrow >= 6 && seatrow <= 11 && seatcol >= 11 && seatcol <= 18
         )
         {
             priceMultiplier *= 2.0; // Multiply the price by 1.5
         }
-        // This is for the orange area 
-        if
-        ( 
-            schedule.Hall_ID == 1 && 
-            seatType == 2 && seatrow >= 3 && seatrow <= 10 && seatcol <= 4 && seatcol >= 3 ||
-            seatrow >= 3 && seatrow <= 10 && seatcol <= 8 && seatcol >= 7 ||
-            seatrow >= 9 && seatrow <= 10 && seatcol >= 5 && seatcol <= 6 ||
-            seatrow >= 3 && seatrow <= 4 && seatcol >= 5 && seatcol <= 6 || 
-            schedule.Hall_ID == 2 &&                     
-            seatType == 2 &&
-            seatrow >= 1 && seatrow <= 15 && seatcol >= 6 && seatcol <= 11 ||
-            seatrow >= 2 && seatrow <= 13 && seatcol >= 5 && seatcol <= 12 ||
-            seatrow >= 4 && seatrow <= 12 && seatcol >= 4 && seatcol <= 13 ||
-            seatrow >= 6 && seatrow <= 11 && seatcol >= 3 && seatcol <= 14 ||
-            seatrow >= 8 && seatrow <= 10 && seatcol >= 2 && seatcol <= 15 ||
-            schedule.Hall_ID == 3 && 
-            seatType == 2 &&                    
-            seatrow >= 1 && seatrow <= 16 && seatcol >= 12 && seatcol <= 17 ||
-            seatrow >= 1 && seatrow <= 15 && seatcol >= 9 && seatcol <= 20 ||
-            seatrow >= 2 && seatrow <= 13 && seatcol >= 8 && seatcol <= 21 ||
-            seatrow >= 4 && seatrow <= 11 && seatcol >= 7 && seatcol <= 22 ||
-            seatrow >= 6 && seatrow <= 10 && seatcol >= 6 && seatcol <= 23 ||
-            seatrow >= 8 && seatrow <= 9 && seatcol >= 5 && seatcol <= 24
+        // This is for the orange area
+        if (
+            schedule.Hall_ID == 1
+                && seatType == 2
+                && seatrow >= 3
+                && seatrow <= 10
+                && seatcol <= 4
+                && seatcol >= 3
+            || seatrow >= 3 && seatrow <= 10 && seatcol <= 8 && seatcol >= 7
+            || seatrow >= 9 && seatrow <= 10 && seatcol >= 5 && seatcol <= 6
+            || seatrow >= 3 && seatrow <= 4 && seatcol >= 5 && seatcol <= 6
+            || schedule.Hall_ID == 2
+                && seatType == 2
+                && seatrow >= 1
+                && seatrow <= 15
+                && seatcol >= 6
+                && seatcol <= 11
+            || seatrow >= 2 && seatrow <= 13 && seatcol >= 5 && seatcol <= 12
+            || seatrow >= 4 && seatrow <= 12 && seatcol >= 4 && seatcol <= 13
+            || seatrow >= 6 && seatrow <= 11 && seatcol >= 3 && seatcol <= 14
+            || seatrow >= 8 && seatrow <= 10 && seatcol >= 2 && seatcol <= 15
+            || schedule.Hall_ID == 3
+                && seatType == 2
+                && seatrow >= 1
+                && seatrow <= 16
+                && seatcol >= 12
+                && seatcol <= 17
+            || seatrow >= 1 && seatrow <= 15 && seatcol >= 9 && seatcol <= 20
+            || seatrow >= 2 && seatrow <= 13 && seatcol >= 8 && seatcol <= 21
+            || seatrow >= 4 && seatrow <= 11 && seatcol >= 7 && seatcol <= 22
+            || seatrow >= 6 && seatrow <= 10 && seatcol >= 6 && seatcol <= 23
+            || seatrow >= 8 && seatrow <= 9 && seatcol >= 5 && seatcol <= 24
         )
         {
             priceMultiplier *= 1.5;
@@ -145,7 +171,7 @@ public class Ticket
         }
     }
 
-    public static void DisplayTicketDetails( int seatType ,int row , int col,double price)
+    public static void DisplayTicketDetails(int seatType, int row, int col, double price)
     {
         string stoelType;
         switch (seatType)
@@ -207,6 +233,7 @@ public class Ticket
             Console.ResetColor();
         }
     }
+
     public static bool UserTicketDiscount(int userID)
     {
         using DataBaseConnection db = new();
@@ -216,14 +243,20 @@ public class Ticket
         if (!user.DiscountReceived)
         {
             List<Ticket> userTickets = db.Ticket.Where(t => t.User_ID == userID).ToList();
-            int totalTicketsBought = userTickets.Count;
+            int totalVisits = user.Visits;
+            List<int> wantedTotalVisits = new List<int> { 11, 21, 31, 41, 51, 61, 71, 81, 91 };
 
-            if (totalTicketsBought > 10)
+            foreach (int visit in wantedTotalVisits)
             {
-                Console.WriteLine("Gefeliciteerd je ontvangt korting op je volgende bestelling!");
-                user.DiscountReceived = true;
-                db.SaveChanges();
-                return true;
+                if (totalVisits == visit)
+                {
+                    Console.WriteLine(
+                        "Gefeliciteerd je ontvangt korting op je volgende bestelling!"
+                    );
+                    user.DiscountReceived = true;
+                    db.SaveChanges();
+                    return true;
+                }
             }
         }
 
