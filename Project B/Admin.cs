59592d271d5490f74b21c843e2f954f8ee1ecb33
JemuelVP <jemuelvalpoort10@gmail.com>
2024-05-21 21:25:39 +0200

    updated admin and user login functionality so that it ask the credentials again

--- Project B/Admin.cs
+++ Project B/Admin.cs
@@ -18,9 +18,9 @@ public class Admin
         admin = new Users();
         hallController = new CinemaHallController(db);
     }
+
     private void SetSelectedAdminOption()
     {
-        
         var choices = new List<AdminChoices>
         {
             AdminChoices.FilmToevoegen,
@@ -33,16 +33,24 @@ public class Admin
         SelectedAdminOption = AnsiConsole.Prompt(
             new SelectionPrompt<AdminChoices>()
                 .Title($"[blue]Welkom {admin.Name} wat wilt u doen?[/]")
-                .AddChoices(
-                    choices
-                )
+                .AddChoices(choices)
         );
     }
+
     public void Run()
     {
-        var name = AnsiConsole.Prompt(new TextPrompt<string>("Voer je gebruikersnaam in: "));
-        var password = AnsiConsole.Prompt(new TextPrompt<string>("Voer je wachtwoord in: ").Secret());
-        admin.Login(name, password);
+        bool LoggedIn = false;
+
+        while (!LoggedIn)
+        {
+            var name = AnsiConsole.Prompt(new TextPrompt<string>("Voer je gebruikersnaam in: "));
+            var password = AnsiConsole.Prompt(
+                new TextPrompt<string>("Voer je wachtwoord in: ").Secret()
+            );
+
+            LoggedIn = admin.Login(name, password);
+        }
+
         while (true)
         {
             SetSelectedAdminOption();
@@ -52,15 +60,15 @@ public class Admin
             }
             if (SelectedAdminOption == AdminChoices.UitLoggen)
             {
-                            var choice = AnsiConsole.Prompt(
-            new SelectionPrompt<Logout>()
-                .Title("[red]Weet je zeker dat je wilt uitloggen?[/]")
-                .AddChoices(Logout.Yes, Logout.No)
-            );
-            if (choice == Logout.Yes)
-            {
-                break;
-            }
+                var choice = AnsiConsole.Prompt(
+                    new SelectionPrompt<Logout>()
+                        .Title("[red]Weet je zeker dat je wilt uitloggen?[/]")
+                        .AddChoices(Logout.Yes, Logout.No)
+                );
+                if (choice == Logout.Yes)
+                {
+                    break;
+                }
             }
             switch (SelectedAdminOption)
             {
@@ -79,6 +87,7 @@ public class Admin
             }
         }
     }
+
     private void FilmToevoegen()
     {
         string title = AnsiConsole.Ask<string>("Titel:");
@@ -89,9 +98,7 @@ public class Admin
         string directors = AnsiConsole.Ask<string>("Directeuren: ");
         int age = AnsiConsole.Ask<int>("Minimale leeftijd: ");
         int durationInMin = AnsiConsole.Ask<int>("Duurt in (minuten): ");
-        AnsiConsole.Write(
-            new Rule("[green]Film is toegevoegd [/]").RuleStyle("green")
-        );
+        AnsiConsole.Write(new Rule("[green]Film is toegevoegd [/]").RuleStyle("green"));
         // Call the AddMovie method with the input parameters
         adminController.AddMovie(
             title,
@@ -104,14 +111,12 @@ public class Admin
             durationInMin
         );
     }
+
     private void GeplandeFilms()
     {
         DateTime startDate = DateTime.Now;
         DateTime endDate = DateTime.Now.AddDays(28);
-        var schedules = ScheduleController.GetAllSchedules(
-        startDate,
-        endDate
-        );
+        var schedules = ScheduleController.GetAllSchedules(startDate, endDate);
 
         // Display available films
         AnsiConsole.Write(
@@ -119,82 +124,79 @@ public class Admin
                 $"[blue]Beschikbare Films Van {startDate.Date.ToShortDateString()} Tot {endDate.Date.ToShortDateString()}:[/]"
             ).RuleStyle("blue")
         );
-        var movies = schedules
-            .Select(s => $"{s.Film.Title} - {s.StartDate}")
-            .ToList();
+        var movies = schedules.Select(s => $"{s.Film.Title} - {s.StartDate}").ToList();
         foreach (var movie in movies)
             AnsiConsole.WriteLine(movie);
     }
+
     private void FilmsOverZicht()
     {
         var adminOverview = AdminController.GetAllMovies();
         string[] movieTitle = adminOverview
-        .Select(book => $"{book.Title}")
-        .OrderBy(title => title)  // Sort titles alphabetically
-        .ToArray();
+            .Select(book => $"{book.Title}")
+            .OrderBy(title => title) // Sort titles alphabetically
+            .ToArray();
         var overviewMovies = AnsiConsole.Prompt(
-            new SelectionPrompt<string>()
-                .Title("Kies een film :")
-                .AddChoices(movieTitle)
+            new SelectionPrompt<string>().Title("Kies een film :").AddChoices(movieTitle)
         );
         // Get the total price for the selected movie
         string titlePart = overviewMovies.Replace("Titel: ", "");
         var selectedMovie = AdminController.GetMovieByTitle(titlePart);
         var ScheduleMovie = AnsiConsole.Prompt(
-        new SelectionPrompt<RevenueOrScheduleMovie>()
-            .Title("[green]Wat wilt u nu doen[/]")
-            .AddChoices(
-                RevenueOrScheduleMovie.TotaleOmzet,
-                RevenueOrScheduleMovie.ScheduleMovie
-            )
+            new SelectionPrompt<RevenueOrScheduleMovie>()
+                .Title("[green]Wat wilt u nu doen[/]")
+                .AddChoices(
+                    RevenueOrScheduleMovie.TotaleOmzet,
+                    RevenueOrScheduleMovie.ScheduleMovie
+                )
         );
         switch (ScheduleMovie)
         {
-        case RevenueOrScheduleMovie.TotaleOmzet:
-            Console.ForegroundColor = ConsoleColor.Blue;
-            AnsiConsole.WriteLine(
-                $"Total Tickets: {RevenueStatistics.GetTotalTicketsPerMovie(selectedMovie.ID)}"
-            );
-            AnsiConsole.WriteLine(
-                $"Totale Omzet: {RevenueStatistics.GetTotalPricePerMovie(selectedMovie.ID)}"
-            );
-            break;
-        case RevenueOrScheduleMovie.ScheduleMovie:
-            Console.WriteLine(
-                "Voer een datum in ANSI-formaat in (dd-MM-jjjj HH:mm):"
-            );
-            string? userInput = Console.ReadLine();
-            if (userInput == null)
+            case RevenueOrScheduleMovie.TotaleOmzet:
+                Console.ForegroundColor = ConsoleColor.Blue;
+                AnsiConsole.WriteLine(
+                    $"Total Tickets: {RevenueStatistics.GetTotalTicketsPerMovie(selectedMovie.ID)}"
+                );
+                AnsiConsole.WriteLine(
+                    $"Totale Omzet: {RevenueStatistics.GetTotalPricePerMovie(selectedMovie.ID)}"
+                );
                 break;
-            var date = DateTime.ParseExact(
-                userInput,
-                "dd-MM-yyyy HH:mm",
-                null,
-                System.Globalization.DateTimeStyles.None
-            );
-            var halls = hallController.GetAllHalls();
-            string[] hallNames = halls.Select(hall => hall.Name).ToArray();
-            var selectedHallName = AnsiConsole.Prompt(
-                new SelectionPrompt<string>().AddChoices(hallNames)
-            );
-            var selectedHall = hallController.GetByName(selectedHallName);
-            if (selectedHall == null)
-                break;
-            var schedule = new Schedule();
-            schedule.CreateFromFilm(selectedMovie, selectedHall.ID, date);
+            case RevenueOrScheduleMovie.ScheduleMovie:
+                Console.WriteLine("Voer een datum in ANSI-formaat in (dd-MM-jjjj HH:mm):");
+                string? userInput = Console.ReadLine();
+                if (userInput == null)
+                    break;
+                var date = DateTime.ParseExact(
+                    userInput,
+                    "dd-MM-yyyy HH:mm",
+                    null,
+                    System.Globalization.DateTimeStyles.None
+                );
+                var halls = hallController.GetAllHalls();
+                string[] hallNames = halls.Select(hall => hall.Name).ToArray();
+                var selectedHallName = AnsiConsole.Prompt(
+                    new SelectionPrompt<string>().AddChoices(hallNames)
+                );
+                var selectedHall = hallController.GetByName(selectedHallName);
+                if (selectedHall == null)
+                    break;
+                var schedule = new Schedule();
+                schedule.CreateFromFilm(selectedMovie, selectedHall.ID, date);
 
-            Console.ReadKey();
-            break;
+                Console.ReadKey();
+                break;
         }
     }
+
     private void Omzet()
     {
         var money = new RevenueStatistics();
         money.GetTotalRevenue();
     }
+
     private void UitLoggen()
     {
-            var choice = AnsiConsole.Prompt(
+        var choice = AnsiConsole.Prompt(
             new SelectionPrompt<Logout>()
                 .Title("[red]Weet je zeker dat je wilt uitloggen?[/]")
                 .AddChoices(Logout.Yes, Logout.No)
@@ -204,6 +206,7 @@ public class Admin
             break;
         }
     }
+
     public enum AdminChoices
     {
         FilmToevoegen,
@@ -213,6 +216,7 @@ public class Admin
         UitLoggen,
         Back
     }
+
     public enum RevenueOrScheduleMovie
     {
         TotaleOmzet,
