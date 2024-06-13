db8d0e20d00d783aeea4ccb2fc50e69d563d2410
JemuelVP <jemuelvalpoort10@gmail.com>
2024-06-13 15:48:04 +0200

    added home screen

--- Project B/Program.cs
+++ Project B/Program.cs
@@ -4,11 +4,20 @@ bool active = true;
 DataBaseConnection db = new();
 
 var hallsController = new CinemaHallController(db);
-AnsiConsole.Write(new Rule($"Welkom bij YourEyes").RuleStyle("blue"));
+
+// AnsiConsole.Write(new Rule($"Welkom bij YourEyes").RuleStyle("blue"));
 var CustomerMenu = new Customer();
 var AdminMenu = new Admin();
 while (active)
 {
+    var font = FigletFont.Load("3d.flf");
+    Console.Clear();
+    AnsiConsole.Write(new FigletText(font, "Welkom bij").Centered().Color(Color.Blue));
+    AnsiConsole.Write(new FigletText(font, "Your Eyes\n").Centered().Color(Color.Blue));
+
+    AnsiConsole.Write(new Rule("[blue]Druk op enter om door te gaan[/]").RuleStyle("blue"));
+    Console.ReadLine();
+    Console.Clear();
     var MainMenuOption = AnsiConsole.Prompt(
         new SelectionPrompt<MainMenuOptions>()
             .Title("[blue]Bent u een admin of een klant[/]")
